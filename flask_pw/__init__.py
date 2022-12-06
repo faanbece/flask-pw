9b5cff58cbb912ea1dca76b6cbe2111512e43322
Fabian Betancur <faanbece@gmail.com>
2022-12-05 19:58:23 -0500

    Revert models search

--- flask_pw/__init__.py
+++ flask_pw/__init__.py
@@ -102,12 +102,12 @@ class Peewee(object):
             try:
                 mod = import_module(self.app.config['PEEWEE_MODELS_MODULE'])
                 for model_name in dir(mod):
-                    model = getattr(mod, model_name)
-                    if not isinstance(model, pw.ModelBase):
+                    models = getattr(mod, model_name)
+                    if not isinstance(model_name, pw.ModelBase):
                         continue
-                    models.append(model)
+                    models.append(model_name)
             except ImportError:
-                return model
+                return models
         elif isinstance(Model_, BaseSignalModel):
             models = BaseSignalModel.models
 
@@ -124,7 +124,7 @@ class Peewee(object):
                         migrate_table=self.app.config['PEEWEE_MIGRATE_TABLE'])
 
         if auto:
-            auto = self.models
+            _ = self.models
 
         router.create(name, auto=auto)
 
